878b6fd0571fa3669731f17cdb26f5645a620997
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-05-24 20:56:48 -0700

    Add favorite function

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
-import photos from "mocks/photos";
-
-
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
   /* Insert React */
   const photo = props.data;
   return (
     <div key={photo.id} className="photo-list__item">
+          <PhotoFavButton/>
       <img className="photo-list__image" src={photo.urls.regular} />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={photo.user.profile} />
