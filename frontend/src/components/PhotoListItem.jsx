75858b4962eec817850b5fd6e9c8bb95b5f7797b
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-05-24 20:03:15 -0700

    Apply SCSS to PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,15 +7,17 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   /* Insert React */
-  return (<div className="PhotoListItem">
-    <img id={props.id} src={props.imageSource}></img>
-    <img id={props.id + 'p'} src={props.profile}></img>
-    <br/>
-    <username>{props.username}</username>
-    <br/>
-    <location>{props.location.city}, {props.location.country}</location>
-  </div>
-
+  return (
+    <div className="photo-list__item">
+      <img className="photo-list__image" id={props.id} src={props.imageSource} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" id={props.id + 'p'} src={props.profile} />
+        <div className="photo-list__user-info">
+          <div>{props.username}</div>
+          <div className="photo-list__user-location">{props.location.city}, {props.location.country}</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
